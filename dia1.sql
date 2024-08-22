a47b0443b0582d472fc186591bc4e7b3dd7c64a9
Lucas Malves <lmalves10@gmail.com>
2024-08-22 10:40:59 -0300

    Resolvendo os 10 primeiros do dia1

--- dia1.sql
+++ dia1.sql
@@ -1,24 +1,50 @@
 /* Atividade solicitas via chatgpt para pratica de sql com o banco de dados*/ 
 
+USE PRATICANDO;
+
+SELECT * FROM clientes;
+SELECT * FROM servicos;
+SELECT * FROM utilizacoes;
+
 /* 1. Selecione todos os clientes que nasceram antes de 1980. */
+
+SELECT Nome, DataNasc as 'Data de Nascimento' from clientes where DataNasc < '1980-01-01';
 
 /* 2. Liste os nomes e telefones dos clientes que moram em "São Paulo". */
 
+SELECT Nome, Telefone, Cidade FROM clientes WHERE Cidade = 'São Paulo';
+
 /* 3. Selecione todos os serviços cujo valor seja maior que '200.00'. */
+
+SELECT NomeServ, Valor FROM servicos WHERE Valor > 200.00;
 
 /* 4. Encontre todos os clientes cujo nome começa com a letra "A". */
 
+SELECT Nome FROM clientes WHERE Nome LIKE 'A%';
+
 /* 5. Selecione todas as utilizações em que o serviço utilizado tem o número "3". */
+
+SELECT * FROM utilizacoes WHERE Ordem LIKE '%3%';
 
 /* 6. Liste todos os clientes do sexo feminino (Sexo = 'F'). */
 
+SELECT Nome, Sexo FROM clientes WHERE Sexo = 'F';
+
 /* 7. Selecione o nome dos clientes que têm "Silva" no sobrenome. */
+
+SELECT Nome FROM clientes WHERE Nome LIKE '%Silva';
 
 /* 8. Encontre todas as utilizações feitas por clientes do sexo masculino (Sexo = 'M'). */
 
+SELECT Ordem, Cliente FROM utilizacoes WHERE Cliente IN (SELECT Cadastro FROM clientes WHERE Sexo = 'M');
+
 /* 9. Selecione todos os serviços cujo valor esteja entre '100.00' e '300.00'. */
 
+SELECT * FROM servicos WHERE Valor > 100.00 AND Valor < 300.00;
+
 /* 10. Liste todos os clientes nascidos após 1º de janeiro de 1990. */
+
+SELECT Nome, DataNasc as 'Data de Nascimento' from clientes where DataNasc > '1990-01-01';
 
 /* 11. Encontre o nome e cidade dos clientes que moram em cidades que começam com "C". */
 
