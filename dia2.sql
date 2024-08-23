86dce4846f6b96b4594519a3465c3c76051abc59
Lucas Malves <lmalves10@gmail.com>
2024-08-23 01:15:11 -0300

    criando arquivo do dia 2

---
+++ dia2.sql
@@ -0,0 +1,67 @@
+/* Atividade solicitas via chatgpt para pratica de sql com o banco de dados*/ 
+
+USE PRATICANDO;
+
+SELECT * FROM clientes;
+SELECT * FROM servicos;
+SELECT * FROM utilizacoes;
+
+/* 1. Selecione todos os clientes que nasceram antes de 1980 e moram em "São Paulo". */
+
+/* 2. Liste os clientes do sexo masculino que têm "Silva" como sobrenome e nasceram depois de 1990. */
+
+/* 3. Selecione todos os serviços cujo valor seja maior que '100.00' e menor que '300.00'. */
+
+/* 4. Encontre os clientes que têm "A" no primeiro nome e moram em "Rio de Janeiro". */
+
+/* 5. Selecione todas as utilizações onde o número de ordem é maior que 500 e o serviço é "3". */
+
+/* 6. Liste os clientes do sexo feminino que moram em cidades diferentes de "São Paulo". */
+
+/* 7. Selecione todos os serviços cujo nome começa com "Servico" e o valor é maior que '200.00'. */
+
+/* 8. Encontre os clientes que têm "M" no nome e são do sexo masculino. */
+
+/* 9. Selecione todos os clientes que nasceram entre 1970 e 1990 e moram em "Belo Horizonte". */
+
+/* 10. Liste os serviços que têm valor diferente de '150.00' e nome termina com "2". */
+
+/* 11. Selecione todas as utilizações onde o cliente tem um cadastro menor que 200 e o serviço custa menos de '200.00'. */
+
+/* 12. Encontre os clientes cujo telefone começa com "(11)" e moram em "Campinas". */
+
+/* 13. Liste os serviços cujo valor é maior que '250.00' ou menor que '100.00'. */
+
+/* 14. Selecione os clientes que nasceram antes de 1985 e moram em cidades cujo nome começa com "C". */
+
+/* 15. Encontre as utilizações onde o cliente é do sexo feminino e o serviço custa mais que '300.00'. */
+
+/* 16. Liste os clientes cujos nomes começam com "J" e o sobrenome com "S". */
+
+/* 17. Selecione todos os serviços cujo nome contém "Serv" e o valor não seja '200.00'. */
+
+/* 18. Encontre os clientes que nasceram entre 1960 e 1980 e têm "Moura" como sobrenome. */
+
+/* 19. Selecione todas as utilizações onde o número de ordem é maior que 800 ou o serviço é "2". */
+
+/* 20. Liste os clientes que moram em cidades diferentes de "Salvador" e cujo nome contém "o". */
+
+/* 21. Encontre os serviços que têm valor menor que '100.00' e o nome tem "3". */
+
+/* 22. Selecione os clientes que têm "Souza" no sobrenome ou moram em "Recife". */
+
+/* 23. Liste todas as utilizações onde o cliente tem cadastro entre 50 e 150 e o serviço é maior que 5. */
+
+/* 24. Selecione os clientes cujos nomes começam com "F" e moram em cidades que terminam com "o". */
+
+/* 25. Encontre os serviços que têm valor maior que '300.00' e o nome contém "Servico". */
+
+/* 26. Liste os clientes que nasceram antes de 1995 e cujo telefone começa com "(21)". */
+
+/* 27. Selecione as utilizações onde o cliente é do sexo masculino e o número de ordem é menor que 300. */
+
+/* 28. Encontre os clientes cujos nomes têm mais de 8 caracteres e moram em cidades cujo nome contém "a". */
+
+/* 29. Liste os serviços cujo valor é exatamente '100.00' ou '200.00' e o nome termina com "4". */
+
+/* 30. Selecione os clientes que nasceram depois de 2000 e moram em "Manaus" ou "Goiânia". */
