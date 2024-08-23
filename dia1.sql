c8c90f116e3b4b4453392e825722108dbb590f12
Lucas Malves <lmalves10@gmail.com>
2024-08-23 01:11:54 -0300

    dia1 concluido

--- dia1.sql
+++ dia1.sql
@@ -48,40 +48,80 @@ SELECT Nome, DataNasc as 'Data de Nascimento' from clientes where DataNasc > '19
 
 /* 11. Encontre o nome e cidade dos clientes que moram em cidades que começam com "C". */
 
+SELECT Nome, Cidade FROM Clientes WHERE Cidade LIKE 'C%';
+
 /* 12. Selecione todas as utilizações feitas por clientes nascidos antes de 1970. */
+
+SELECT Ordem, Cliente FROM utilizacoes WHERE Cliente IN (SELECT Cadastro FROM clientes WHERE DataNasc < '1970-01-01'); 
 
 /* 13. Liste todos os serviços cujo nome termina com "1". */
 
+SELECT NomeServ FROM Servicos WHERE `NomeServ` LIKE '%1';
+
 /* 14. Encontre todos os clientes que têm "M" como inicial do primeiro nome. */
+
+SELECT Nome FROM clientes WHERE `Nome` LIKE 'M%';
 
 /* 15. Selecione os clientes cujos telefones contêm o código de área "11". */
 
+SELECT Nome, Telefone FROM clientes WHERE `Telefone` LIKE '(11)%';
+
 /* 16. Liste todos os serviços cujo valor não seja igual a '150.00'. */
+
+SELECT NomeServ, `Valor` FROM servicos WHERE `Valor` <> 150.00;
 
 /* 17. Encontre todos os clientes que moram em cidades diferentes de "Rio de Janeiro". */
 
+SELECT `Cadastro`, Nome, Cidade FROM clientes WHERE `Cidade` <> 'Rio de Janeiro';
+
 /* 18. Selecione os serviços que têm um valor menor que '250.00'. */
+
+SELECT NomeServ, Valor FROM servicos WHERE `Valor` < 250.00;
 
 /* 19. Liste as utilizações em que o cliente tenha o número de cadastro maior que 200. */
 
+SELECT Ordem, Cliente FROM utilizacoes WHERE `Cliente` IN (SELECT Cadastro FROM clientes WHERE `Cadastro` > 200);
+
 /* 20. Encontre todos os clientes que têm "Santos" como sobrenome. */
+
+SELECT Nome FROM clientes WHERE `Nome` LIKE '%Santos';
 
 /* 21. Selecione todos os serviços cujo valor seja exatamente '300.00'. */
 
+SELECT NomeServ, Valor FROM servicos WHERE `Valor` = 300.00;
+
 /* 22. Liste todos os clientes que moram em cidades que terminam com "o". */
+
+SELECT Nome, Cidade FROM clientes WHERE `Cidade` like '%o';
 
 /* 23. Encontre todas as utilizações onde o número de ordem é maior que 500. */
 
+SELECT Ordem, Cliente FROM utilizacoes WHERE `Ordem` > 500;
+
 /* 24. Selecione todos os clientes cujo nome completo contém a letra "r". */
+
+SELECT Nome FROM clientes WHERE `Nome` like '%r%';
 
 /* 25. Liste todos os serviços cujo nome tem exatamente 9 caracteres. */
 
+SELECT NomeServ FROM servicos WHERE LENGTH(NomeServ) = 9;
+
 /* 26. Encontre todos os clientes que nasceram entre 1950 e 1970. */
+
+SELECT Nome, DataNasc FROM clientes WHERE `DataNasc` > '1950-01-01' AND `DataNasc` < '1970-01-01';
 
 /* 27. Selecione as utilizações em que o serviço utilizado tem um valor menor que '100.00'. */
 
+SELECT Ordem, Servico FROM utilizacoes WHERE `Servico` IN (SELECT NumServ FROM servicos WHERE `Valor` < 100.00);
+
 /* 28. Liste todos os clientes cujos nomes têm mais de 10 caracteres. */
+
+SELECT Nome FROM clientes WHERE LENGTH(`Nome`) > 10;
 
 /* 29. Encontre os serviços que têm "Serv" no nome. */
 
+SELECT NomeServ FROM servicos WHERE `NomeServ` LIKE 'Serv%';
+
 /* 30. Selecione todas as utilizações onde o cliente tem "Carla" como primeiro nome. */
+
+SELECT Ordem, Cliente FROM utilizacoes WHERE `Cliente` IN (SELECT Cadastro FROM clientes WHERE `Nome` like 'Carla%');
